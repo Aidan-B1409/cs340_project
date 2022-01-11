9778ae4cf8be17798d3b0d0779583f3e0e03cf20
Aidan-B1409 <=>
2022-01-10 17:40:50 -0800

    Updated credentials

--- db-connector.js
+++ db-connector.js
@@ -6,7 +6,7 @@ var pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'classmysql.engr.oregonstate.edu',
     user            : 'cs340_beerya',
-    password        : 'SkoBeaves128',
+    password        : '1299',
     database        : 'cs340_beerya'
 })
 
