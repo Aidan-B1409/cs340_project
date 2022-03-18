7341d69b31b8a2373fa2eb14562aa528461c19f1
Aidan Beery <beerya@flip1.engr.oregonstate.edu>
2022-03-17 18:50:38 -0700

    full CRUD functionality

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from flask_mysqldb import MySQL
 from flask import request
 import database.db_connector as db
 
-db_connection = db.connect_to_database()
-
 app = Flask(__name__)
 
 
@@ -20,9 +18,10 @@ def root():
     }
     return render_template("main.j2", pages=pages)
 
-# TODO - More Routes!
+
 @app.route('/university', methods=['POST', 'GET'])
 def university():
+    db_connection = db.connect_to_database()
     if(request.method == 'GET'):
         query = "SELECT * FROM university"
         cursor = db.execute_query(db_connection=db_connection, query=query)
@@ -50,8 +49,45 @@ def university():
             _ = db.execute_query(db_connection=db_connection, query=query)
             return redirect('/university')
 
+@app.route('/delete_university/<int:id>')
+def delete_university(id):
+    db_connection = db.connect_to_database()
+    query = f"DELETE FROM university WHERE id = \"{id}\""
+    _ = db.execute_query(db_connection=db_connection, query=query)
+    return redirect('/university')
+
+@app.route('/update_university/<int:id>', methods=['POST', 'GET'])
+def update_university(id):
+    db_connection = db.connect_to_database()
+    if request.method == 'GET':
+        query = f"SELECT * FROM university WHERE id = {id}"
+        cursor = db.execute_query(db_connection=db_connection, query=query)
+        results = cursor.fetchone()
+        return render_template("edit_university.j2", form_fields=results)
+    if request.method == 'POST':
+        if request.form.get('university-update'):
+            student_count = request.form["student_count"]
+            ranking = request.form["ranking"]
+            name = request.form["name"]
+            est_date = request.form["est_date"]
+            street_name = request.form["street_name"]
+            street_num = request.form["street_num"]
+            zip_code = request.form["zip_code"]
+
+            query = f"UPDATE university SET university.student_count = {int(student_count)}, \
+                                            university.ranking = {int(ranking)}, \
+                                            university.name = \"{name}\", \
+                                            university.est_date = \"{est_date}\", \
+                                            university.street_name = \"{street_name}\", \
+                                            university.street_num = {int(street_num)}, \
+                                            university.zip_code = {int(zip_code)} \
+                                            WHERE university.id = {int(id)}"
+            _ = db.execute_query(db_connection=db_connection, query=query)
+            return redirect("/university")
+
 @app.route('/faculty', methods=['POST', 'GET'])
 def faculty():
+    db_connection = db.connect_to_database()
     if(request.method == 'GET'):
         query = "SELECT * FROM faculty"
         cursor = db.execute_query(db_connection=db_connection, query=query)
@@ -84,8 +120,48 @@ def faculty():
             _ = db.execute_query(db_connection=db_connection, query=add_faculty)
             return redirect('/faculty')
 
+
+@app.route('/delete_faculty/<int:id>')
+def delete_faculty(id):
+    db_connection = db.connect_to_database()
+    query = f"DELETE FROM faculty WHERE id = \"{id}\""
+    _ = db.execute_query(db_connection=db_connection, query=query)
+    return redirect('/faculty')
+
+
+@app.route('/update_faculty/<int:id>', methods=['POST', 'GET'])
+def update_faculty(id):
+    db_connection = db.connect_to_database()
+    if request.method == 'GET':
+        query = f"SELECT * FROM faculty WHERE id = {id}"
+        cursor = db.execute_query(db_connection=db_connection, query=query)
+        results = cursor.fetchone()
+        return render_template("edit_faculty.j2", form_fields=results)
+    if request.method == 'POST':
+        if request.form.get('faculty-update'):
+            first_name = request.form["first_name"]
+            last_name = request.form["last_name"]
+            date_of_birth = request.form["date_of_birth"]
+            street_name = request.form["street_name"]
+            street_number = request.form["street_num"]
+            zip_code = request.form["zip_code"]
+            pid = request.form["pid"]
+
+            query = f"UPDATE faculty SET faculty.first_name = \"{first_name}\", \
+                                         faculty.last_name = \"{last_name}\", \
+                                         faculty.date_of_birth = \"{date_of_birth}\", \
+                                         faculty.street_name = \"{street_name}\", \
+                                         faculty.street_num = {int(street_number)}, \
+                                         faculty.zip_code = {int(zip_code)}, \
+                                         faculty.pid = {int(pid)} \
+                                         WHERE faculty.id = {int(id)}"
+            _ = db.execute_query(db_connection=db_connection, query=query)
+            return redirect("/faculty")
+
+
 @app.route('/course', methods=['POST', 'GET'])
 def course():
+    db_connection = db.connect_to_database()
     if(request.method == 'GET'):
         query = "SELECT * FROM course"
         cursor = db.execute_query(db_connection=db_connection, query=query)
@@ -131,9 +207,115 @@ def course():
                 VALUES (\"{course_code}\", \"{course_name}\", \"{course_desc}\", \"{start_date}\", \"{end_date}\", \"{start_time}\", \"{end_time}\", {pid}, {eid}, {cid})"
             _ = db.execute_query(db_connection=db_connection, query=add_course)
             return redirect("/course")
+        if(request.form.get('course-search')):
+            course_name = request.form['search_class']
+            get_course = f"SELECT * FROM course WHERE course_name = \"{course_name}\""
+            get_course_cursor = db.execute_query(db_connection=db_connection, query=get_course)
+            result = get_course_cursor.fetchone()['id']
+            return redirect(f"/course_search/{result}")
+
+@app.route('/course_search/<int:id>')
+def search_course(id):
+    db_connection = db.connect_to_database()
+    if(request.method == 'GET'):
+            query = f"SELECT * FROM course WHERE id = {id}"
+            cursor = db.execute_query(db_connection=db_connection, query=query)
+            results = cursor.fetchall()
+            form_fields = {'course_code': 'Course Code',
+                        'course_name': 'Course Name',
+                        'course_desc': 'Course Description',
+                        'start_date': 'Start Date',
+                        'end_date': 'End Date',
+                        'start_time': 'Start Time',
+                        'end_time': 'End Time',
+                        'uni_name': 'University Name',
+                        'faculty_name': 'Instructor Last Name',
+                        'major_name': 'Major Name'}
+            return render_template("course.j2", course_info=results, form_fields=form_fields)
+    if(request.method == 'POST'):
+        if request.form.get('course-submit'):
+            course_name = request.form["course_name"]
+            course_code = request.form["course_code"]
+            course_desc = request.form["course_desc"]
+            start_date = request.form["start_date"]
+            end_date = request.form["end_date"]
+            start_time = request.form["start_time"]
+            end_time = request.form["end_time"]
+            uni_name = request.form["uni_name"]
+            faculty_name = request.form["faculty_name"]
+            major_name = request.form["major_name"]
+
+            get_uni_id =  f"SELECT id FROM university WHERE name=\"{uni_name}\""
+            uni_id_cursor = db.execute_query(db_connection=db_connection, query=get_uni_id)
+            pid = uni_id_cursor.fetchone()['id']
+
+            get_major_id = f"SELECT id FROM major WHERE name=\"{major_name}\""
+            major_id_cursor = db.execute_query(db_connection=db_connection, query=get_major_id)
+            cid = major_id_cursor.fetchone()['id']
+
+            get_faculty_id = f"SELECT id FROM faculty WHERE last_name=\"{faculty_name}\""
+            faculty_id_cursor = db.execute_query(db_connection=db_connection, query=get_faculty_id)
+            eid = faculty_id_cursor.fetchone()['id']
+
+            add_course = f"INSERT INTO course(course_code, course_name, course_description, start_date, end_date, start_time, end_time, pid, eid, cid)\
+                VALUES (\"{course_code}\", \"{course_name}\", \"{course_desc}\", \"{start_date}\", \"{end_date}\", \"{start_time}\", \"{end_time}\", {pid}, {eid}, {cid})"
+            _ = db.execute_query(db_connection=db_connection, query=add_course)
+            return redirect("/course")
+        if(request.form.get('course-search')):
+            course_name = request.form['search_class']
+            get_course = f"SELECT * FROM course WHERE name = \"{course_name}\""
+            get_course_cursor = db.execute_query(db_connection=db_connection, query=get_course)
+            result = get_course_cursor.fetchone()['id']
+            return redirect(f"/course_search/{result}")
+
+
+@app.route('/delete_course/<int:id>')
+def delete_course(id):
+    db_connection = db.connect_to_database()
+    query = f"DELETE FROM course WHERE id = \"{id}\""
+    _ = db.execute_query(db_connection=db_connection, query=query)
+    return redirect('/course')
+
+
+@app.route('/update_course/<int:id>', methods=['POST', 'GET'])
+def update_course(id):
+    db_connection = db.connect_to_database()
+    if request.method == 'GET':
+        query = f"SELECT * FROM course WHERE id = {id}"
+        cursor = db.execute_query(db_connection=db_connection, query=query)
+        results = cursor.fetchone()
+        return render_template("edit_course.j2", form_fields=results)
+    if request.method == 'POST':
+        if request.form.get('course-update'):
+            course_name = request.form["course_name"]
+            course_code = request.form["course_code"]
+            course_desc = request.form["course_description"]
+            start_date = request.form["start_date"]
+            end_date = request.form["end_date"]
+            start_time = request.form["start_time"]
+            end_time = request.form["end_time"]
+            pid = request.form["pid"]
+            eid = request.form["eid"]
+            cid = request.form["cid"]
+
+            query = f"UPDATE course SET course.course_name = \"{course_name}\", \
+                                         course.course_code = \"{course_code}\", \
+                                         course.course_description = \"{course_desc}\", \
+                                         course.start_date = \"{start_date}\", \
+                                         course.end_date = \"{end_date}\", \
+                                         course.start_time = \"{start_time}\", \
+                                         course.end_time = \"{end_time}\", \
+                                         course.pid = {int(pid)}, \
+                                         course.eid = {int(eid)}, \
+                                         course.cid = {int(cid)} \
+                                         WHERE course.id = {int(id)}"
+            _ = db.execute_query(db_connection=db_connection, query=query)
+            return redirect("/course")
+
 
 @app.route('/major', methods=['POST', 'GET'])
 def major():
+    db_connection = db.connect_to_database()
     if(request.method == 'GET'):
         query = "SELECT * FROM major"
         cursor = db.execute_query(db_connection=db_connection, query=query)
@@ -158,8 +340,54 @@ def major():
             _ = db.execute_query(db_connection=db_connection, query=add_major)
             return redirect('/major')
 
+
+@app.route('/delete_major/<int:id>')
+def delete_major(id):
+    db_connection = db.connect_to_database()
+    query = f"DELETE FROM major WHERE id = \"{id}\""
+    _ = db.execute_query(db_connection=db_connection, query=query)
+    return redirect('/major')
+
+@app.route('/update_major/<int:id>', methods=['POST', 'GET'])
+def update_major(id):
+    db_connection = db.connect_to_database()
+    if request.method == 'GET':
+        query = f"SELECT * FROM major WHERE id = {id}"
+        cursor = db.execute_query(db_connection=db_connection, query=query)
+        results = cursor.fetchone()
+        return render_template("edit_major.j2", form_fields=results)
+    if request.method == 'POST':
+        if request.form.get('major-update'):
+            name = request.form["name"]
+            major_code = request.form["major_code"]
+            description = request.form["description"]
+            pid = request.form['pid']
+
+            query = f"UPDATE major SET major.name = \"{name}\", \
+                                       major.major_code = {int(major_code)}, \
+                                       major.description = \"{description}\", \
+                                       major.pid = {int(pid)} \
+                                       WHERE major.id = {int(id)}"
+            _ = db.execute_query(db_connection=db_connection, query=query)
+            return redirect("/major")
+
+
+@app.route('/studentcourse')
+def studentcourse():
+    db_connection = db.connect_to_database()
+    if(request.method == 'GET'):
+        query = "SELECT * FROM student_course"
+        cursor = db.execute_query(db_connection=db_connection, query=query)
+        results = cursor.fetchall()
+        form_fields = {
+            'pid': 'Student ID',
+            'eid': 'Major Code'
+        }
+        return render_template("student-course.j2", form_fields=form_fields, student_courses=results)
+
 @app.route('/student', methods=['POST', 'GET'])
 def student():
+    db_connection = db.connect_to_database()
     if(request.method == 'GET'):
         query = "SELECT * FROM student"
         cursor = db.execute_query(db_connection=db_connection, query=query)
@@ -199,7 +427,48 @@ def student():
                 VALUES (\"{first_name}\", \"{last_name}\", \"{date_of_birth}\", {float(gpa)}, \"{street_name}\", {int(street_number)}, {int(zip_code)}, {pid}, {eid})"
             
             _ = db.execute_query(db_connection=db_connection, query=add_student)
-            return redirect('/student')
+            return redirect('/student')            
+        
+
+@app.route('/delete_student/<int:id>')
+def delete_student(id):
+    db_connection = db.connect_to_database()
+    query = f"DELETE FROM student WHERE id = \"{id}\""
+    _ = db.execute_query(db_connection=db_connection, query=query)
+    return redirect('/student')
+
+@app.route('/update_student/<int:id>', methods=['POST', 'GET'])
+def update_student(id):
+    db_connection = db.connect_to_database()
+    if request.method == 'GET':
+        query = f"SELECT * FROM student WHERE id = {id}"
+        cursor = db.execute_query(db_connection=db_connection, query=query)
+        results = cursor.fetchone()
+        return render_template("edit_students.j2", form_fields=results)
+    if request.method == 'POST':
+        if request.form.get('student-update'):
+            first_name = request.form["first_name"]
+            last_name = request.form["last_name"]
+            date_of_birth = request.form["date_of_birth"]
+            gpa = request.form["gpa"]
+            street_name = request.form["street_name"]
+            street_number = request.form["street_num"]
+            zip_code = request.form["zip_code"]
+            pid = request.form["pid"]
+            eid = request.form["eid"]
+
+            query = f"UPDATE student SET student.first_name = \"{first_name}\", \
+                                         student.last_name = \"{last_name}\", \
+                                         student.date_of_birth = \"{date_of_birth}\", \
+                                         student.gpa = {float(gpa)}, \
+                                         student.street_name = \"{street_name}\", \
+                                         student.street_num = {int(street_number)}, \
+                                         student.zip_code = {int(zip_code)}, \
+                                         student.eid = {int(eid)}, \
+                                         student.pid = {int(pid)} \
+                                         WHERE student.id = {int(id)}"
+            _ = db.execute_query(db_connection=db_connection, query=query)
+            return redirect("/student")
 
 # Listener
 if __name__ == "__main__":
